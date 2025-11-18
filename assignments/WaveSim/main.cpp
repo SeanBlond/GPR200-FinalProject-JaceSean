bba74b1e42523813f466fd4f3d0ec45181bab934
Sean <gapammons@gmail.com>
2025-11-18 14:37:18 -0500

    Changed some slight settings

--- assignments/WaveSim/main.cpp
+++ assignments/WaveSim/main.cpp
@@ -94,13 +94,13 @@ int main()
     // Wave Settings
     glm::vec2 direction = glm::vec2(1.0f, 1.0f);
     float wavelength = 1.0f;
-    float steepness = 0.5f;
+    float steepness = 1.0f;
     float speed = 1.0f;
-    int detail = 4;
+    int detail = 2;
 
 
     // Creating the Rendered Object
-    obj::Object wavePlane("plane", mesh::createPlane(8.0f, 8.0f, 32), &waveShader, glm::vec3(0));
+    obj::Object wavePlane("ocean", mesh::createPlane(8.0f, 8.0f, 32), &waveShader, glm::vec3(0));
 
     // Creating pointers to the primitive's settings
     PlaneMesh* wavePlanePtr = dynamic_cast<PlaneMesh*>(wavePlane.getMesh());
